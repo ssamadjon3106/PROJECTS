53726dd18bac1d0255544d2a1f73d1eab4b2a36e
ssamadjon3106 <samadjonsayfullayev3106@gmail.com>
2026-02-12 00:01:38 +0500

    Update app.py

--- foodvision_mini/app.py
+++ foodvision_mini/app.py
@@ -14,53 +14,53 @@ effnetb2, effnet2_transforms=create_effnetb2_model(num_classes=3)
 
 effnetb2.load_state_dict(torch.load(f='09_pretrained_effnetb2_feature_extractor _pizza_steak_sushi_20_percent.pth'), map_location=torch.device('cpu'))
 
-#predict function
+
 def predict(img) -> Tuple[Dict, float]:
     """Transforms and performs a prediction on img and returns prediction and time taken.
     """
-    # Start the timer
+    
     start_time = timer()
     
-    # Transform the target image and add a batch dimension
+    
     img = effnetb2_transforms(img).unsqueeze(0)
     
-    # Put model into evaluation mode and turn on inference mode
+    
     effnetb2.eval()
     with torch.inference_mode():
-        # Pass the transformed image through the model and turn the prediction logits into prediction probabilities
+        
         pred_probs = torch.softmax(effnetb2(img), dim=1)
     
-    # Create a prediction label and prediction probability dictionary for each prediction class (this is the required format for Gradio's output parameter)
+    
     pred_labels_and_probs = {class_names[i]: float(pred_probs[0][i]) for i in range(len(class_names))}
     
-    # Calculate the prediction time
+    
     pred_time = round(timer() - start_time, 5)
     
-    # Return the prediction dictionary and prediction time 
+    
     return pred_labels_and_probs, pred_time
 
 
 example_list=[['examples/'+example] for example in os.listdir('examples')]
 
-### gradio app
+
 import gradio as gr
 
-# Create title, description and article strings
+
 title = "FoodVision Mini 🍕🥩🍣"
 description = "An EfficientNetB2 feature extractor computer vision model to classify images of food as pizza, steak or sushi."
 article = "Created at [09. PyTorch Model Deployment](https://www.learnpytorch.io/09_pytorch_model_deployment/)."
 
-# Create the Gradio demo
-demo = gr.Interface(fn=predict, # mapping function from input to output
-                    inputs=gr.Image(type="pil"), # what are the inputs?
-                    outputs=[gr.Label(num_top_classes=3, label="Predictions"), # what are the outputs?
-                             gr.Number(label="Prediction time (s)")], # our fn has two outputs, therefore we have two outputs
+
+demo = gr.Interface(fn=predict, 
+                    inputs=gr.Image(type="pil"), 
+                    outputs=[gr.Label(num_top_classes=3, label="Predictions"), 
+                             gr.Number(label="Prediction time (s)")], 
                     examples=example_list, 
                     title=title,
                     description=description,
                     article=article)
 
-# Launch the demo!
+
 demo.launch(debug=False) 
 
 
