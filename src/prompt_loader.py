f8d97e40af0dc78007afcc9ea4f046369fcce867
ssamadjon3106 <samadjonsayfullayev3106@gmail.com>
2026-04-22 19:59:31 +0500

    Delete src directory

--- src/prompt_loader.py
+++
@@ -1,9 +0,0 @@
-import yaml
-from pathlib import Path
-
-PROMPT_PATH = Path(__file__).parent / "prompts.yaml"
-
-
-def load_prompts():
-    with open(PROMPT_PATH, "r") as f:
-        return yaml.safe_load(f)
