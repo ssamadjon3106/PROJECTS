bd866833b12bf7d035fa7516f842eb2a148a5781
Samadjon <samadjonsayfullayev3106@gmail.com>
2026-05-07 09:03:06 +0500

    Remove accidental nested repository

--- Library/Library/Real-Time Drowsiness Detection System/src/alarm.py
+++
@@ -1,8 +0,0 @@
-import pygame
-pygame.mixer.init()
-ALARM_SOUND=pygame.mixer.Sound('/Users/samadjon/Documents/projects/Real-Time Drowsiness Detection System/src/alarm.mp3')
-def play_alarm():
-    if not pygame.mixer.get_busy():
-        ALARM_SOUND.play()
-def stop_alarm():
-    pygame.mixer.stop()
