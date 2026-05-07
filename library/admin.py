e3a79ba9757ad35c0d1fe1099bd8c8ea65df99da
ssamadjon3106 <samadjonsayfullayev3106@gmail.com>
2026-05-07 08:49:56 +0500

    Delete library directory

--- library/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-
-# Admin registrations have been moved to their respective functional apps:
-# catalog_app, circulation_app, and seats_app.
--- library/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-
-class BaseSearchForm(forms.Form):
-    """A reusable base search form for different apps."""
-    q = forms.CharField(
-        required=False, 
-        widget=forms.TextInput(attrs={'placeholder': 'Search...', 'class': 'search-input'})
-    )
