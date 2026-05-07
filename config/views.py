550e9a96e4639d7ca1f386aa8a5859a99b1fb47b
ssamadjon3106 <samadjonsayfullayev3106@gmail.com>
2026-05-07 08:48:35 +0500

    Delete config directory

--- config/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def custom_404(request, exception):
-    return render(request, '404.html', status=404)
-
-
-def custom_500(request):
-    return render(request, '500.html', status=500)
